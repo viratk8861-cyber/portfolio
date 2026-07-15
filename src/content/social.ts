13c2b3b0ee6bc3b1909f7a87adedb00c39b15b73
Jigar-kar <virat.k8861@gmail.com>
2026-07-15 18:20:17 +0530

    Update social.ts

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,7 +1,7 @@
 export const social = [
-  { url: "mailto:me@kar-jigar.com", name: "mail" },
-  { url: "https://github.com/karjigar", name: "github" },
-  { url: "https://www.linkedin.com/in/kar-jigar/", name: "linkedin" },
-  { url: "https://x.com/KarJigar", name: "x" },
+  { url: "mailto:karjigar56@gmail.com", name: "mail" },
+  { url: "https://github.com/Jigar-kar", name: "github" },
+  { url: "https://www.linkedin.com/in/jigaar/", name: "linkedin" },
+  { url: "https://x.com/kjigar", name: "x" },
   //{ url: "https://www.instagram.com/karjigar/", name: "instagram" },
 ] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
